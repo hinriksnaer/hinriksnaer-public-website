d2b22958006cbfd6929ee7326bed5bc241fa0b47
softmax <hinriksnaer@gmail.com>
2024-02-04 16:57:45 -0500

    first commit

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-76c27cae0f5cd881df5a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkv4=self.webpackChunkv4||[]).push([[760],{2136:function(e,t,n){var u=n(2411);t.__esModule=!0,t.default=void 0;var r=u(n(2444)),a=u(n(1504)),l=function(e){function t(){return e.apply(this,arguments)||this}return(0,r.default)(t,e),t.prototype.render=function(){return a.default.createElement(a.default.Fragment,null)},t}(a.default.Component),f=l;t.default=f}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-76c27cae0f5cd881df5a.js.map
